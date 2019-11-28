2ab8918b2aaec76f12b99ef624fcfafd31aeba4e
Administrator <Administrator@172.3.21.100>
2019-11-28 20:04:49 +0800

    这是项目

---
+++ src/main/java/com/chinasofti/etc/sso/mapper/SSOClient.java
@@ -0,0 +1,20 @@
+package com.chinasofti.etc.sso.mapper;
+
+public class SSOClient {
+	private int clientid;
+	private String clienturl;
+	public int getClientid() {
+		return clientid;
+	}
+	public void setClientid(int clientid) {
+		this.clientid = clientid;
+	}
+	public String getClienturl() {
+		return clienturl;
+	}
+	public void setClienturl(String clienturl) {
+		this.clienturl = clienturl;
+	}
+	
+
+}
